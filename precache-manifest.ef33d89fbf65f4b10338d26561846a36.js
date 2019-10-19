1775f9af017d2232f9e9146e6052d6b562479f63
Dint <dintccd@gmail.com>
2019-10-19 14:27:07 +0700

    Updates

---
+++ precache-manifest.ef33d89fbf65f4b10338d26561846a36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2cbca93cbcfa101153d57cab476590d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "63b8437c75c600cba19b",
+    "url": "/monsters-rolodex/static/css/main.0501365c.chunk.css"
+  },
+  {
+    "revision": "b0703eff4582a78f493a",
+    "url": "/monsters-rolodex/static/js/2.5075ebff.chunk.js"
+  },
+  {
+    "revision": "63b8437c75c600cba19b",
+    "url": "/monsters-rolodex/static/js/main.5d4c5fbe.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
